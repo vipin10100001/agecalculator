fd3dd8f2246644c2049843c57c8ac1c2fe221736
N. VIPIN PRASAD <75900544+vipin10100001@users.noreply.github.com>
2023-05-18 19:33:05 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-function calculateAge() {
-    const dobInput = document.getElementById("dob").value;
-    const dob = new Date(dobInput);
-    const today = new Date();
-    const age = Math.floor((today - dob) / (365.25 * 24 * 60 * 60 * 1000));
-    document.getElementById("result").innerHTML = `Your age is ${age} years.`;
-  }
-  
